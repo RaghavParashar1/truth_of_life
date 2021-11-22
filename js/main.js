e1dff0f62bbbe2aee9f060cd1a11e43c2923bfe3
RaghavParashar1 <rparasharr@gmail.com>
2021-11-22 23:22:11 +0530

    Added text Animation

--- js/main.js
+++ js/main.js
@@ -91,7 +91,32 @@ icon.onclick = function(){
         icon.title= "pause"
     }else{
         song.pause();
-        icon.src = "Images/pause.png"
+        icon.src = "Images/play.png"
         icon.title= "play"
     }
 }
+
+
+
+// for animation
+
+const texts = ["Friends", "Love", "Money"];
+let count = 0;
+let index = 0;
+let currenttext = '';
+let letter = '';
+
+(function type(){
+    if (count === texts.length){
+        count = 0;
+    }
+    currenttext = texts[count];
+    letter = currenttext.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+    if(letter.length === currenttext.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 400);
+})();
